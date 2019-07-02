594ed0b0c8a6791f56676e334fa226fc1d37534d
IchinoseMasayuki <16ec016@ms.dendai.ac.jp>
2019-07-02 14:37:34 +0900

    tenmura

---
+++ Assets/Scripts/SceneChanger/ToScore4.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToScore4 : MonoBehaviour
+{
+    public void OnClick()
+    {
+        SceneManager.LoadScene("Score_4");
+    }
+}
