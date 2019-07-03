4f868397a43b00eb94e8b3ef4ba6a33be8b1557e
gomiakuta <true.tiriakuta@gmail.com>
2019-07-03 10:32:20 +0900

    ソースコードをコピペ

---
+++ Assets/6/PlayerController_TAMA.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Vector2 mousePos1;
+    Vector2 mousePos2;
+    Vector2 movePos;
+    Unit unit;
+   
+    void Start()
+    {
+        unit = GetComponent<Unit>();
+        this.mousePos1 = Input.mousePosition;
+    }
+    
+    void Update()
+    {
+        MoveSwipe();
+    }
+    
+    void MoveSwipe()
+    {
+        this.mousePos2 = Input.mousePosition;
+
+        float x_swipeLength = mousePos2.x - this.mousePos1.x;
+        float y_swipeLength = mousePos2.y - this.mousePos1.y;
+
+        this.movePos = mousePos2 - mousePos1;
+        unit.Move(movePos);
+        mousePos1 = mousePos2;
+    }
+}
---
+++ Assets/6/ScoreManager_TAMA.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    private float starttime;
+    private float scoretime;
+    private bool flag = false;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        starttime = Time.time;
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (flag)
+        {
+            if (Time.time - timer > 1.0f)
+            {
+                string STAGE_NAME = SceneManager.GetActiveScene().name.Replace("Stage_", "");
+                SceneManager.LoadScene("Score_" + STAGE_NAME.Replace("_hard", ""));
+            }
+        }
+    }
+    public void setScore()
+    {
+        scoretime = Time.time - starttime;
+        //スコアの登録
+        string STAGE_NAME = SceneManager.GetActiveScene().name.Replace("Stage_", "");
+        if(scoretime < PlayerPrefs.GetFloat(STAGE_NAME + "_SCORE"))
+        {
+            PlayerPrefs.SetFloat(STAGE_NAME + "_SCORE", scoretime);
+        }
+        PlayerPrefs.SetInt(STAGE_NAME + "_CLEAR", PlayerPrefs.GetInt(STAGE_NAME + "_CLEAR") + 1);
+                //スコア表示画面に移行（hardの場合文字列からハードを取り除く）  
+        SceneManager.LoadScene(("Score_" + STAGE_NAME).Replace("_hard",""));
+
+        Debug.Log("SET "+STAGE_NAME + "_SCORE->" + PlayerPrefs.GetFloat(STAGE_NAME + "_SCORE"));
+        Debug.Log("SET "+STAGE_NAME + "_CLEAR->" + PlayerPrefs.GetFloat(STAGE_NAME + "_CLEAR"));
+        flag = true;
+        timer = Time.time;
+    }
+    public void backScene()
+    {
+        flag = true;
+        timer = Time.time;
+    }
+}
